e09fc232992294627fecd3e570ccfa42f822e2b8
yavuzerbas <yavuz.erbas@metu.edu.tr>
2023-05-11 12:20:55 +0300

    GUI improvements

--- app/src/main/java/com/example/music/screens/category_screen/view/CategoryScreen.kt
+++ app/src/main/java/com/example/music/screens/category_screen/view/CategoryScreen.kt
@@ -1,11 +1,14 @@
 package com.example.music.screens.category_screen.view
 
 import android.net.Uri
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +18,10 @@ import com.example.music.screens.category_screen.model.Genre
 import com.example.music.screens.category_screen.model.GenresResponse
 import com.google.gson.Gson
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
@@ -45,43 +52,40 @@ fun CategoryScreen(navController: NavController, encodedJsonMyGenres: String) {
         GenresList(genresResponse = myGenres)
     }
 }
-
 @Composable
 fun GenresList(genresResponse: GenresResponse) {
-    LazyColumn {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.padding(4.dp),
+        contentPadding = PaddingValues(4.dp)
+    ) {
         items(genresResponse.data) { genre ->
-            GenreCard(genre)
+            genreBox(genre)
         }
     }
 }
-
 @Composable
-fun GenreCard(genre: Genre) {
-    Card(
+fun genreBox(genre: Genre){
+    Box(
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth(),
-        elevation = 8.dp
+            .fillMaxWidth().
+            clip(RoundedCornerShape(10.dp))
     ) {
-        Column(
+        AsyncImage(model = genre.pictureBig, contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
+        Text(
+            text = genre.name,
             modifier = Modifier
-                .padding(16.dp)
-        ) {
-            AsyncImage(model = genre.picture, contentDescription = null, modifier = Modifier.align(
-                Alignment.CenterHorizontally)
-            )
-            Text(text = genre.name)
-        }
+                .align(Alignment.BottomCenter)
+                .background(Color.Black.copy(alpha = 0.7f))
+                .padding(8.dp).
+                fillMaxWidth()
+            ,
+            color = androidx.compose.ui.graphics.Color.White,
+            style = TextStyle(shadow = Shadow(color = Color.Black, blurRadius = 10f))
+        )
     }
 }
 
-//List of boxes in a column
-@Composable
-fun CategoryColumn() {
-
-}
-
-@Composable
-fun CategoryBox(){
-
-}
